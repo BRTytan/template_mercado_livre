da704f06bc1d36b55d760e782199075fb6e0bc2f
BRTytan <fe-santos00@hotmail.com>
2022-04-02 12:43:00 -0300

    include template em html utilizando flask

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
+@app.route("/")
+def template_test():
     
     return render_template('index.html')
+
+# @app.route('/')
+# def hello():
+    
+#     return render_template('template.html')
