a3c08ca04367be60991ef869f65f6fb44b2f00d5
BRTytan <fe-santos00@hotmail.com>
2022-04-02 12:49:13 -0300

    liberando modo debug no main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from doctest import DebugRunner
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,9 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def template_test():
     
-    return render_template('index.html')
+    return render_template('index.html',site="Mercado- LIvre fake")
 
 # @app.route('/')
 # def hello():
     
 #     return render_template('template.html')
+
+app.run(debug=True)
