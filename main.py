01a88e863581cb4479e14b1a07fe75b4da0320a3
BRTytan <fe-santos00@hotmail.com>
2022-04-04 01:36:09 -0300

    Aplicando loop no jinja

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from doctest import DebugRunner
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    
-    return render_template('index.html',site="Mercado- LIvre fake")
+    home="historico"
+    file = open('historico.json','r').read()
+    data = json.loads(file)
+    return render_template('index.html',items=data,site="Mercado - Livre fake",home=home)
 
-# @app.route('/')
-# def hello():
-    
-#     return render_template('template.html')
+@app.route('/historico')
+def hello():
+    file = open('historico.json','r').read()
+    data = json.loads(file)
+    return render_template('historico.html',items=data)
 
 app.run(debug=True)
+
+
+
